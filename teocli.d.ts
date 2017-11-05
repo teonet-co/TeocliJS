c105a6fab3b56327fac43038312d637b5b511101
Kirill Scherba <kirill@scherba.ru>
2017-11-06 02:51:27 +0300

    Typescript type definition added

--- teocli.d.ts
+++ teocli.d.ts
@@ -4,6 +4,7 @@
 
 export default class Teocli {
 
+    CMD: any;
     ws: WebSocket;
     client_name: string;
 
@@ -12,8 +13,14 @@ export default class Teocli {
     onopen(ev: any): void;
     onclose(ev: any): void;
     onerror(ev: any): void;
-    onother(err: any, data: any): number;
+    onother(err: any, data: any): number;    
+    onecho(err: any, data: any): number;
+    onclients(err: any, data: any): number;
 
+    process(data:any): number;
+    
     login(client_name: string): void;
     echo(to: string, msg: string): void;
+    clients(to: string): void;
+    send(data: any): boolean;
 }
